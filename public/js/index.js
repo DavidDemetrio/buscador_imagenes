407f5dc5750633ed2d867fef92a0d188ffad0f23
DavidDemetrio <davidlopez.paz26@outlook.es>
2023-06-03 00:54:18 -0600

    Se termina funcionalidad para obtener registro de busqueda desde el API y mostrar los resultados en cards con paginacion usando generadores.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
-const registrosPorPagina = 40;
-let paginaActual = 1;
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+const paginacionDiv = document.querySelector('#paginacion');
+const REGISTROS_POR_PAGINA = 40;
+let paginaActual = 1;
+let totalPaginas = 0;
 
 const mostrarMensajeAlerta = () => {
     const mensajeAlerta = document.createElement('DIV');
@@ -8,7 +11,7 @@ const mostrarMensajeAlerta = () => {
     mensajeAlerta.classList.add('bg-red-100', 'border', 'border-red-400', 'text-sm', 'text-red-700', 'msg-alert',
     'm-auto', 'mt-2', 'px-4', 'py-px', 'rounded', 'relative', 'animate__animated', 'animate__bounceIn');
     
-    mensajeAlerta.style = 'width: 32rem';
+    mensajeAlerta.style = 'width: 18rem';
     mensajeAlerta.role = 'alert';
     mensajeAlerta.innerHTML = `
         <strong class="font-bold">Opps!</strong>
@@ -16,6 +19,87 @@ const mostrarMensajeAlerta = () => {
     `;
     const divBuscador = document.querySelector('.buscador');
     divBuscador.after(mensajeAlerta);
+    setTimeout(() => {
+        mensajeAlerta.remove();
+    }, 2500);
+}
+
+const calcularPaginas = total => {
+    return parseInt(Math.ceil(total / REGISTROS_POR_PAGINA));
+}
+
+const buscarImagenes = () => {
+    const key = document.querySelector('#key-pixaby').value;
+    const busqueda = document.querySelector('#termino').value;
+    const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${REGISTROS_POR_PAGINA}&page=${paginaActual}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+function *crearPaginador(total) {
+    for (let i = 1; i <= total; i++ ) {
+        yield i;
+    }
+}
+
+const imprimirPaginador = () => {
+    const iterador = crearPaginador(totalPaginas);
+    while (true) {
+        const { value, done } = iterador.next();
+        if (done) return;
+        // Caso contrario, genera un botón por cada elemento en el generador
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-transparent', 'shadow-md', 'hover:bg-blue-500', 'text-blue-700', 'font-semibold',
+        'hover:text-white', 'py-1', 'px-4', 'mr-2', 'mb-4', 'border', 'border-blue-400', 'hover:border-transparent', 'rounded');
+        boton.onclick = () => {
+            paginaActual = value;
+            buscarImagenes();
+        }
+        paginacionDiv.appendChild(boton);
+    }
+}
+
+const mostrarImagenes = imagenes => {
+    // Limpiar el contenedor donde se muestran las imágenes de registros anteriores
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imágnes y construir el HTML
+    imagenes.forEach(imagen => {
+        const { webformatURL, likes, views, largeImageURL } = imagen;
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${webformatURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Me Gusta </span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Veces Vista </span> </p>
+
+                        <a
+                            class="block w-full bg-blue-800 hover:bg-blue-700 uppercase font-bold text-center rounded mt-5 p-1 text-white"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer" 
+                        >Ver Imagen</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+    // Limpiar el paginador previo
+    while(paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild)
+    }
+    // Generamos el nuevo HTML
+    imprimirPaginador();
 }
 
 const getImagenesAPI = e => {
@@ -32,14 +116,7 @@ const getImagenesAPI = e => {
     */
     // Elimina cualquier mensaje de alerta
     if (existe_mensajeAlerta) existe_mensajeAlerta.remove();
-    const key = document.querySelector('#key-pixaby').value;
-    const urlAPI = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${registrosPorPagina}&page=${paginaActual}`;
-
-    fetch(urlAPI)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            console.log(resultado);
-        })
+    buscarImagenes();
 }
 
 window.onload = () => {
